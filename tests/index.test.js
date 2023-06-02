959b60bd72e952573c23f86037886fbe042ce7ec
Arnoldo Magaña <elswordmon@gmail.com>
2023-06-01 18:49:53 -0600

    Two test added for the methods createMatriz and initMatriz
    Co-authored-by: landillo <orlando.leso@gmail.com>
    Co-authored-by: AlbertoG22 <albertogarciamartinez99@gmail.com>
    Co-authored-by: Alfredo20164920 <apaz20164920@gmail.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,10 +2,32 @@ const JuegoDeLaVida = require("../src/index");
 
 describe("Pruebas del archivo /.index.js", () => {
   const juego = new JuegoDeLaVida(4, 8);
-  test("probar el método createMatriz()", () => {
+  describe("createMatriz()", () => {
     const matriz = juego.createMatriz();
 
     expect(typeof juego.createMatriz).toBe("function");
     expect(typeof matriz).toBe("object");
+
+    it("iniciar matriz con ceros", () => {
+      const juego = new JuegoDeLaVida(2, 2);
+      const matriz = juego.createMatriz();
+
+      expect(matriz).toEqual([[0, 0], [0, 0]]);
+    });
+  });
+
+  describe("initMatriz", () => {
+    it("inicar con valores al azar de 0 y 1", () => {
+      const juego = new JuegoDeLaVida(4, 8);
+      juego.initMatriz();
+      const matriz = juego.cuadricula;
+
+      for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+          expect(matriz[i][j]).toBeGreaterThanOrEqual(0);
+          expect(matriz[i][j]).toBeGreaterThanOrEqual(1);
+        }
+      }
+    });
   });
 });
